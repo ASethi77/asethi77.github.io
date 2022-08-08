19819a1f9cc5962ce7c2d8942347aec5b6b574d3
Aakash Sethi <aakash.k.sethi@gmail.com>
2022-08-08 00:54:50 -0700

    Replace with react + tailwind

---
+++ full-of-empty-space/src/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class NavBar extends React.Component {
+    render() {
+        return (
+        <nav class="px-2 bg-white border-gray-200 dark:bg-gray-900 dark:border-gray-700">
+            <button class="btn">Button</button>
+        </nav>
+        );
+    }
+}
+
+export default NavBar;
