6a46f23d5eaee3a85a6bd0460192bb8cb4c2527b
Aakash Sethi <aakash.k.sethi@gmail.com>
2022-12-22 01:26:27 -0800

    fix mobile padding

--- dynamic-seal/pages/blog/[...slug].vue
+++ dynamic-seal/pages/blog/[...slug].vue
@@ -1,7 +1,7 @@
 <template>
-  <div class="bg-background-50 min-h-screen p-12">
+  <div class="bg-background-50 min-h-screen">
     <NavBar></NavBar>
-    <div class="font-blog-post mx-auto w-full md:w-2/3">
+    <div class="font-blog-post mx-auto w-full md:w-2/3 p-12">
       <ContentDoc v-slot="{ doc }" class="">
           <h1 class="my-12 xl:mt-24 xl:mb-16 text-4xl xl:text-6xl text-red-500 font-semibold font-heading">{{  doc.title  }}</h1>
           <ContentRenderer :value="doc"></ContentRenderer>
