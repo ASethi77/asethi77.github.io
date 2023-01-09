feebc2c21023e3439de916ad7eea15a27da9cea1
Aakash Sethi <aakash.k.sethi@gmail.com>
2023-01-08 23:26:40 -0800

    Fix

--- dynamic-seal/components/NavBar.vue
+++ dynamic-seal/components/NavBar.vue
@@ -2,7 +2,7 @@
 <nav class="font-heading text-3xl lg:text-4xl text-foreground-50 bg-background-50 mb-12">
     <div class="pt-6 px-12 flex items-center justify-between text-foreground-50" :class="{'flex-wrap': isMobile, 'menu-selected': menuActive }">
   <div class="flex items-center flex-shrink-0 mr-6">
-    <button class="text-2xl lg:text-3xl mr-8 lg:mr-12" type="button" @click=nightwind.toggle()><font-awesome-icon icon="far fa-moon" /></button>
+    <button class="text-2xl lg:text-3xl mr-8 lg:mr-12" :class="{ '!text-foreground-900': menuActive }" type="button" @click=nightwind.toggle()><font-awesome-icon icon="far fa-moon" /></button>
     <RouterLink :to="'/'"><span class="tracking-tight" :class="{ '!text-foreground-900': menuActive }">Dynamic Seal</span></RouterLink>
   </div>
   <div class="flex flex-wrap ml-auto">
