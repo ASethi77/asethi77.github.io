0123618b3141b56f69c1a7eaa74cf97e6f095f06
Aakash Sethi <aakash.k.sethi@gmail.com>
2023-04-17 20:03:39 -0700

    Update theme color

--- dynamic-seal/app.vue
+++ dynamic-seal/app.vue
@@ -4,3 +4,14 @@
     <NuxtPage />
   </div>
 </template>
+
+<script setup lang="ts">
+useHead({
+  title: 'Dynamic Seal',
+  meta: [
+    // I don't know why Nuxt won't let me set theme-color through nuxt.config.js, but this page
+    // said useHead() in app.vue was a new recommended interface and it seems to work ¯\_(ツ)_/¯
+    { name: 'theme-color', content: '#111827' }
+  ],
+})
+</script>
--- dynamic-seal/nuxt.config.js
+++ dynamic-seal/nuxt.config.js
@@ -54,7 +54,6 @@ export default defineNuxtConfig({
     head: {
         meta: [
             { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-            { name: 'theme-color', content: '#111827' }
         ]
     },
     target: 'static',
