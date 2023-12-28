5d59481e90928d7c8151c5521aa467794a3b89e1
Aakash Sethi <aakash.k.sethi@gmail.com>
2023-12-27 19:58:12 -0800

    fix styles

--- dynamic-seal/pages/blog/index.vue
+++ dynamic-seal/pages/blog/index.vue
@@ -2,17 +2,17 @@
     <div>
         <NavBar></NavBar>
         <div class="bg-background-50 min-h-screen p-12">
-            <div class="mx-auto w-full md:w-2/3">
-                <h1 class="font-heading text-6xl lg:text-7xl text-foreground-50 mt-24 mb-12">Posts</h1>
+            <div class="mx-auto w-full lg:w-1/2">
+                <h1 class="font-heading text-4xl lg:text-6xl text-foreground-50 mt-24 mb-12">Posts</h1>
                 <div class="post min-h-24 my-12 w-full rounded-lg" v-for="post in postList">
                     <RouterLink v-if="post['published']" :to="post['_path']">
                         <div class="flex flex-col leading-[4rem] md:flex-row justify-between items-start" >
-                            <h2 class="inline my-auto text-4xl text-red-500 font-semibold">
+                            <h2 class="inline my-auto text-2xl lg:text-3xl text-red-500 font-semibold">
                                 <span class="leading-none align-bottom ">
                                     {{post['title']}}
                                 </span>
                             </h2>
-                            <h3 class="my-auto text-xl text-rose-800 md:text-right">
+                            <h3 class="my-auto text-xl lg:text-2xl text-red-700 md:text-right">
                                 <span class="leading-none align-center md:align-bottom ">
                                     {{ post['date'] }}
                                     <font-awesome-icon class="ml-2" icon="fa-regular fa-calendar"></font-awesome-icon>
@@ -20,7 +20,7 @@
                             </h3>
                         </div>
                         <div class="mt-4 text-foreground-50">
-                            <p>{{post['description']}}</p>
+                            <p class="text-lg lg:text-xl">{{post['description']}}</p>
                         </div>
                     </RouterLink>
                 </div>
