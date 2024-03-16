0ff0a4aa784fe360e724f4ff72e589c707373d5b
Aakash Sethi <aakash.k.sethi@gmail.com>
2024-03-16 16:51:50 -0700

    remove broken plugin

--- dynamic-seal/plugins/exceleditor.js
+++
@@ -1,7 +0,0 @@
-// import { defineNuxtPlugin } from '#app'
-// import { VueExcelEditor, VueExcelColumn } from 'vue3-excel-editor'
-
-// export default defineNuxtPlugin((nuxtApp) => {
-//   nuxtApp.vueApp.component('vue-excel-editor', VueExcelEditor, {})
-//   nuxtApp.vueApp.component('vue-excel-column', VueExcelColumn, {})
-// })
